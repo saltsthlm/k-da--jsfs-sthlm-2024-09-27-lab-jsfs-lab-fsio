b02fc564152eaffc741eaea215ec37d1ad314b1b
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-03 17:02:16 +0200

    add-lint-and-new-lint-rules
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- fs-regex-be/.eslintrc.js
+++ fs-regex-be/.eslintrc.js
@@ -8,4 +8,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  rules: {
+  "semi": "warn",
+  "eqeqeq": "warn",
+  "no-unused-vars": "warn",
+  "prefer-const": "warn"
+}
 }
